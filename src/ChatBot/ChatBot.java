cdbd9101e6cba97f52b87a5582bef8f55bd9888b
AlexanderLukash <sashass20062727@gmail.com>
2024-09-30 08:57:24 +0300

    ChatBot 2-nd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,5 +1,7 @@
 package ChatBot;
 
+import java.util.Scanner;
+
 public class ChatBot {
     public static void main(String[] args) {
         String botName = "DICTBot";
@@ -7,5 +9,18 @@ public class ChatBot {
 
         System.out.println("Hello! My name is " + botName + ".");
         System.out.println("I was created in " + birthYear + ".");
+        System.out.println("Please, remind me your name.");
+
+        // Додаємо символ ">" для позначення введення користувачем
+        System.out.print("> ");
+
+        // Створення об'єкта для зчитування введення від користувача
+        Scanner scanner = new Scanner(System.in);
+
+        String yourName = scanner.nextLine(); // Зчитуємо ім'я користувача
+        System.out.println("What a great name you have, " + yourName + "!");
+
+        scanner.close();
+
     }
 }
