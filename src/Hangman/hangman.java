7131a125249bd485c4f7aeab3eca0eb7d86b16f1
AlexanderLukash <sashass20062727@gmail.com>
2024-10-08 17:13:20 +0300

    1-st Stage

---
+++ src/Hangman/hangman.java
@@ -0,0 +1,8 @@
+package Hangman;
+
+public class hangman {
+    public static void main(String[] args) {
+        System.out.println("HANGMAN");
+        System.out.println("The game will be available soon.");
+    }
+}
