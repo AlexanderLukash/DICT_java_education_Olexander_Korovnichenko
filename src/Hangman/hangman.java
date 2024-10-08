a77bfc8de73a3f6eddffe8c2737079b66fc26257
AlexanderLukash <sashass20062727@gmail.com>
2024-10-08 17:15:29 +0300

    2-nd Stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -1,8 +1,23 @@
 package Hangman;
 
+import java.util.Scanner;
+
 public class hangman {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String secretWord = "java"; // слово, яке гравець повинен вгадати
+
         System.out.println("HANGMAN");
-        System.out.println("The game will be available soon.");
+        System.out.print("Guess the word:> ");
+        String userGuess = scanner.nextLine();
+
+        if (userGuess.equalsIgnoreCase(secretWord)) {
+            System.out.println("You survived!");
+        } else {
+            System.out.println("You lost!");
+        }
+
+        scanner.close();
     }
 }
