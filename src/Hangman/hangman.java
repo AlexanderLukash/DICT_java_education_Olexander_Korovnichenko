62e45b2fc2218e9d2b66dc053bf69e7a966e8e64
AlexanderLukash <sashass20062727@gmail.com>
2024-10-08 17:40:51 +0300

    8-th Stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -7,24 +7,36 @@ import java.util.Scanner;
 public class hangman {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Список слів для гри
         String[] words = {"python", "java", "javascript", "kotlin"};
-
-        // Вибір випадкового слова зі списку
         Random random = new Random();
-        String secretWord = words[random.nextInt(words.length)];
-
-        // Створення масиву для збереження вгаданих букв
-        StringBuilder guessedLetters = new StringBuilder("-".repeat(secretWord.length()));
-        HashSet<Character> guessedChars = new HashSet<>(); // Множина для збереження вгаданих букв
-        int attemptsLeft = 8; // Кількість помилок, які можна допустити
 
         System.out.println("HANGMAN");
 
+        while (true) {
+            System.out.print("Type \"play\" to play the game, \"exit\" to quit: ");
+            String choice = scanner.nextLine();
+
+            if (choice.equals("play")) {
+                playGame(scanner, words[random.nextInt(words.length)]);
+            } else if (choice.equals("exit")) {
+                System.out.println("Goodbye!");
+                break;
+            } else {
+                System.out.println("Invalid choice. Please type \"play\" or \"exit\".");
+            }
+        }
+
+        scanner.close();
+    }
+
+    private static void playGame(Scanner scanner, String secretWord) {
+        StringBuilder guessedLetters = new StringBuilder("-".repeat(secretWord.length()));
+        HashSet<Character> guessedChars = new HashSet<>();
+        int attemptsLeft = 8;
+
         while (attemptsLeft > 0) {
             System.out.println(guessedLetters);
-            System.out.print("Input a letter: > ");
+            System.out.print("Input a letter: ");
             String userInput = scanner.nextLine().toLowerCase();
 
             // Перевірка на валідність введення
@@ -52,14 +64,12 @@ public class hangman {
 
             // Перевірка наявності букви в слові
             if (secretWord.indexOf(guessedLetter) >= 0) {
-                // Якщо буква є в слові, розкриваємо її
                 for (int i = 0; i < secretWord.length(); i++) {
                     if (secretWord.charAt(i) == guessedLetter) {
                         guessedLetters.setCharAt(i, guessedLetter);
                     }
                 }
             } else {
-                // Якщо буква відсутня, зменшуємо кількість спроб
                 attemptsLeft--;
                 System.out.println("That letter doesn't appear in the word.");
             }
@@ -69,18 +79,12 @@ public class hangman {
                 System.out.println(guessedLetters);
                 System.out.println("You guessed the word " + secretWord + "!");
                 System.out.println("You survived!");
-                break;
+                return;
             }
 
-            // Вивід кількості спроб, що залишилися
             System.out.println("Attempts left: " + attemptsLeft);
         }
 
-        if (attemptsLeft == 0) {
-            System.out.println("You lost! The word was: " + secretWord);
-        }
-
-        System.out.println("Thanks for playing!");
-        scanner.close();
+        System.out.println("You lost! The word was: " + secretWord);
     }
 }
