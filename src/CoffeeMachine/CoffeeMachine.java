c3bdc5827e165d4bd85faba9c1d9769ef88c1255
AlexanderLukash <sashass20062727@gmail.com>
2024-10-16 11:08:28 +0300

    2-nd Stage.

--- src/CoffeeMachine/CoffeeMachine.java
+++ src/CoffeeMachine/CoffeeMachine.java
@@ -1,18 +1,35 @@
 package CoffeeMachine;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
 
-    public static void main(String[] args) {
-        CoffeeMachine coffeeMachine = new CoffeeMachine();
-        coffeeMachine.makeEspresso();
+    // Константи для кількості інгредієнтів на одну чашку кави
+    private static final int WATER_PER_CUP = 200; // мл води
+    private static final int MILK_PER_CUP = 50;   // мл молока
+    private static final int COFFEE_BEANS_PER_CUP = 15; // г кавових зерен
+
+    // Метод для розрахунку інгредієнтів
+    public void calculateIngredients(int cups) {
+        int totalWater = WATER_PER_CUP * cups;
+        int totalMilk = MILK_PER_CUP * cups;
+        int totalCoffeeBeans = COFFEE_BEANS_PER_CUP * cups;
+
+        System.out.println("For " + cups + " cups of coffee you will need:");
+        System.out.println(totalWater + " ml of water");
+        System.out.println(totalMilk + " ml of milk");
+        System.out.println(totalCoffeeBeans + " g of coffee beans");
     }
 
-    public void makeEspresso() {
-        System.out.println("Starting to make a coffee");
-        System.out.println("Grinding coffee beans");
-        System.out.println("Boiling water");
-        System.out.println("Mixing boiled water with crushed coffee beans");
-        System.out.println("Pouring coffee into the cup");
-        System.out.println("Coffee is ready!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        CoffeeMachine coffeeMachine = new CoffeeMachine();
+
+        // Запит у користувача кількості чашок кави
+        System.out.println("Write how many cups of coffee you will need:");
+        int cups = scanner.nextInt();
+
+        // Розрахунок інгредієнтів
+        coffeeMachine.calculateIngredients(cups);
     }
 }
