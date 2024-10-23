c20e68e78e5f5f1f41fa725f6daea1b565c4ebd7
AlexanderLukash <sashass20062727@gmail.com>
2024-10-23 19:45:32 +0300

    3-rd Stage.

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -16,6 +16,60 @@ public class TicTacToe {
         System.out.println("---------");
     }
 
+    // Метод для перевірки виграшних комбінацій
+    public static boolean checkWinner(char[][] board, char symbol) {
+        // Перевірка рядків, стовпчиків та діагоналей
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == symbol && board[i][1] == symbol && board[i][2] == symbol) return true;
+            if (board[0][i] == symbol && board[1][i] == symbol && board[2][i] == symbol) return true;
+        }
+        if (board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol) return true;
+        return board[0][2] == symbol && board[1][1] == symbol && board[2][0] == symbol;
+    }
+
+    // Метод для перевірки стану гри
+    public static String analyzeGame(String input) {
+        char[][] board = new char[3][3];
+        int xCount = 0;
+        int oCount = 0;
+
+        // Заповнюємо дошку і рахуємо кількість "X" та "O"
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = input.charAt(i * 3 + j);
+                if (board[i][j] == 'X') xCount++;
+                if (board[i][j] == 'O') oCount++;
+            }
+        }
+
+        boolean xWins = checkWinner(board, 'X');
+        boolean oWins = checkWinner(board, 'O');
+        boolean hasEmptyCells = input.contains("_");
+
+        // Перевірка на неможливий стан гри
+        if ((xWins && oWins) || Math.abs(xCount - oCount) > 1) {
+            return "Impossible";
+        }
+
+        // Перевірка на перемогу X
+        if (xWins) {
+            return "X wins";
+        }
+
+        // Перевірка на перемогу O
+        if (oWins) {
+            return "O wins";
+        }
+
+        // Перевірка на незавершену гру
+        if (hasEmptyCells) {
+            return "Game not finished";
+        }
+
+        // Якщо всі клітинки заповнені й немає переможця
+        return "Draw";
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -37,5 +91,9 @@ public class TicTacToe {
 
         // Виведення ігрового поля
         printBoard(input);
+
+        // Аналіз стану гри
+        String result = analyzeGame(input);
+        System.out.println(result);
     }
 }
